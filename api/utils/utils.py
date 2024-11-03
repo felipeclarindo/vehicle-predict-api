82e10c4043685238787688ed2e487a2e720db066
felipeclarindo <fglpc2@gmail.com>
2024-11-03 14:26:59 -0300

    Adicionando funções utilitarias

---
+++ api/utils/utils.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+def check_folder(folder_name: str) -> bool:
+    try:
+        if Path.exists(Path(__file__).resolve().parent.parent.parent / folder_name):
+            return True
+        raise FileNotFoundError("Falha ao carregar o modelo. (Diretório não encontrado)")
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+    return False
+
+def create_folder(folder_name: str) -> None:
+    print(Path(__file__).resolve().parent.parent.parent / folder_name)
+    if not Path.exists(Path(__file__).resolve() / folder_name):
+        Path.touch(folder_name)
